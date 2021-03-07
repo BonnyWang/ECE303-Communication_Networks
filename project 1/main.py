52cd531f67b1fd9c88194de3ff844b0786e2e74c
BonnyWang <bonny.y.gardy@gmail.com>
2021-03-07 15:13:36 -0500

    TODO: Need UDP Scan too?

---
+++ project 1/main.py
@@ -0,0 +1,36 @@
+import sys 
+import socket 
+
+   
+   
+# Solve the input from the user
+if len(sys.argv) > 2:
+    if sys.argv[2] =='-p':
+        portRange = sys.argv[3].split(":")
+
+        if len(portRange) == 1:
+            portRange.append(portRange[0])
+
+        #Type conversion
+        portRange[0] = int(portRange[0])
+        portRange[1] = int(portRange[1])
+    else:
+        print("WARN: Invalid Input!")
+else:
+    
+    # No port specified 
+    portRange = [1,1024]
+
+host = socket.gethostbyname(sys.argv[1])  
+
+
+for port in range(portRange[0], portRange[1]+1):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(1)
+
+    result = s.connect_ex((host,port))
+    if result == 0:
+        print("Port " + str(port) +" is open" + " with service " + socket.getservbyport(port))
+
+
+s.close()
